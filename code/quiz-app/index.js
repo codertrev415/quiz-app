c3e24d7de1b8b1ea855b97dfc1a21d6821fe6c56
Trevor Antonio <codertrev@proton.me>
2026-06-15 22:39:03 -0700

    Add nested for loops to dynamically render questions and answers to DOM

--- code/quiz-app/index.js
+++ code/quiz-app/index.js
@@ -6,46 +6,49 @@ const quiz = [
 
 function addElement(){
 
-    let createDiv = document.createElement("div")
-    document.body.appendChild(createDiv)
-    createDiv.textContent = (quiz[0].question)
-      
-    let button = document.createElement("button")
-    createDiv = document.createElement("div")
-    button = document.createElement("button")
-    document.body.appendChild(createDiv)
-    document.body.appendChild(button)
-    createDiv.textContent = (quiz[0].answers)
-    button.appendChild(createDiv)
-    button.addEventListener("click", () => {
-        alert("")
-    //button.appendChild(button)
-    })
-
-    createDiv = document.createElement("div")
-    button = document.createElement("button")
-    document.body.appendChild(createDiv)
-    document.body.appendChild(button)
-    createDiv.textContent = (quiz[0].correct)
-    button.appendChild(createDiv)
-    button.addEventListener("click", () => {
-        alert("")
-    button.appendChild(button)
-    })
-
     
+
     for(let i = 0; i < quiz.length; i++){
-    let currentQuestion = quiz[i]
-    console.log(`Question ${i + 1}: ${currentQuestion.question}`)
-    console.log(`Options: ${currentQuestion.answers.join(", ")}`)
-    console.log(`The correct answer is: ${currentQuestion.correct}`)
+        let currentQuestion = quiz[i]
+        let createSection = document.createElement("section")
+        let createDiv = document.createElement("div")
+        createSection.appendChild(createDiv)
+        createDiv.textContent = (currentQuestion.question)
+        document.body.appendChild(createSection)
+        
+        for(let i = 0; i < currentQuestion.answers.length; i++){
+            
 
-    for(let i = 0; i < AudioScheduledSourceNode)
-
-
+            let button = document.createElement("button")
+            createDiv = document.createElement("div")
+            document.body.appendChild(createDiv)
+            document.body.appendChild(button)
+            createDiv.textContent = (currentQuestion.answers[i])
+            button.appendChild(createDiv)
+            button.addEventListener("click", () => {
+                const button = document.querySelector("answers")
+                button.classList.toggle("hidden")
+                
+    //button.appendChild(button)
+            })
+    
+        
+    }
 } 
+    }
    
-}
+
 
 addElement()
 
+
+const button = document.querySelector("button")
+button.classList.add("bold")
+button.addEventListener("click", () =>{
+    button.classList.toggle("hidden")
+})
+/*console.log(button)
+
+console.log(`Question ${i + 1}: ${currentQuestion.question}`)
+    console.log(`Options: ${currentQuestion.answers.join(", ")}`)
+    console.log(`The correct answer is: ${currentQuestion.correct}`)*/
